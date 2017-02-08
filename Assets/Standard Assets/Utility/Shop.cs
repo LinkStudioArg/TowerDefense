861a1673ad9cbd88bbda2ff8afe31a705796283f
Lucas Vignoli <holdinait@gmail.com>
2017-02-08 19:59:40 -0300

    no message

---
+++ Assets/Standard Assets/Utility/Shop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Shop : MonoBehaviour {
+
+	private Vector2 nodePosition;
+    public GameObject selectedItem;
+    public GameObject shopPanel;
+
+    public void SetSelectedItem(GameObject selectedItem)
+    {
+        this.selectedItem = selectedItem;
+    }
+
+    public void SetNodePosition(Vector2 pos)
+    {
+        this.nodePosition = pos;
+    }
+    public void Show()
+    {
+        Debug.Log(nodePosition);
+        shopPanel.SetActive(true);
+    }
+
+    public void CreateSelectedItem()
+    {
+        if (selectedItem != null)
+        {
+            GameObject.Instantiate(selectedItem, nodePosition, Quaternion.identity);
+        }
+    }
+
+}
